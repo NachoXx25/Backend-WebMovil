d6964ea2fd1162c0cfe82359c881ed96462771a6
NachoXx25 <105187185+NachoXx25@users.noreply.github.com>
2024-03-29 19:14:34 -0300

    Build: basic classes user, role, product and add some dependencies

---
+++ Src/Data/DataContext.cs
@@ -0,0 +1,17 @@
+using Microsoft.EntityFrameworkCore;
+using taller1WebMovil.Src.Models;
+
+namespace taller1WebMovil.Src.Data
+{
+    public class DataContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+        public DbSet<Role> Roles { get; set; }
+        public DbSet<Product> Products { get; set; }
+
+        public DataContext(DbContextOptions options) : base(options)
+        {
+            
+        }
+    }
+}
--- obj/Debug/net8.0/taller1WebMovil.AssemblyInfo.cs
+++ obj/Debug/net8.0/taller1WebMovil.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("taller1WebMovil")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e568867337556038e74912173486e0402a87e924")]
 [assembly: System.Reflection.AssemblyProductAttribute("taller1WebMovil")]
 [assembly: System.Reflection.AssemblyTitleAttribute("taller1WebMovil")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
