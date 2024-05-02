821c1f5426b30dc97d6ca414a4d60a54a5afec44
Katapentakill <manesillasdelreloj@gmail.com>
2024-05-01 21:39:13 -0400

    chore: Merge changes from develop

--- obj/Debug/net8.0/taller1WebMovil.AssemblyInfo.cs
+++ obj/Debug/net8.0/taller1WebMovil.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("taller1WebMovil")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d641a08694e109f56afb2c84e17d9d815657c9e8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8da4074818e050b3ae963a3aa17887fc256eb8c8")]
 [assembly: System.Reflection.AssemblyProductAttribute("taller1WebMovil")]
 [assembly: System.Reflection.AssemblyTitleAttribute("taller1WebMovil")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
